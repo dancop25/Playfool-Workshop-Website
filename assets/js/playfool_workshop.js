ee13b255e6d289e05e07d45b9e1c4cd833f4de86
Dan <kimera@hotmail.com>
2018-02-04 21:47:30 +0000

    Many adjustments
    - Fixed section title spacings
    - Cleaned up Jquery
    - Made Squiggly more dynamic
    - Adjusted banner
    - Fixed pricing section bugs

--- assets/js/playfool_workshop.js
+++ assets/js/playfool_workshop.js
@@ -1,6 +1,16 @@
 $(document).ready(function() { 
 
+
+// Variables
+// ------------------------------------
+
+	var isMobile = false; //initiate as false
+    // device detection
+    if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) 
+        || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4))) isMobile = true; 	
+
 	var $window = $(window),
+	$block = $(".block"),
     $block_inner = $(".block_inner"),
     $course_desc_workshop = $(".-workshop > .course_desc"),
     $course_desc_facilitator = $(".-facilitator > .course_desc"),
@@ -8,26 +18,25 @@ $(document).ready(function() {
 	frequency,
 	amplitude,
 	squiggleHeight,
-
-	
 	p = function (x,y) {
 		return { x:x, y:y };
 	};	
 
-	// Redraw Squiggly Line on browser resize. 
-	$window.resize(function () {
-        drawSquigglyLine();
-        changeWorkshopHeight();
-    });
 
-	
+// Functions
+// ------------------------------------
+
+    // Changing Height of Workshop Box
 	function changeWorkshopHeight() {
-		var facilitator_height = $course_desc_facilitator.css('height');
-	    $course_desc_workshop.css('height', facilitator_height);
+		if ($window.width() >= 768) {
+			var facilitator_height = $course_desc_facilitator.css('height');
+		    $course_desc_workshop.css('height', facilitator_height);
+		} else {
+			$course_desc_workshop.css('height', 'auto');
+		}
 	}
-	changeWorkshopHeight();
 
-
+	// Squiggly Line
 	CanvasRenderingContext2D.prototype.wavy = function(from, to, frequency, amplitude, step, negative) 
 	{ 
 		var cx = 0, cy = 0, 
@@ -49,28 +58,31 @@ $(document).ready(function() {
 		}
 	}
 
-
 	function drawSquigglyLine() {
 		$(".squiggly_line").each(function(i, data) {
-			lineWidth = 5;
-			amplitude = 5;
-			squiggleHeight = (amplitude * 2) + lineWidth + 1;
-
-			// if (squigClass == ".shortSquig1") {
-			// 	$squigCanvas.css('width', 175);
-			// } else {
-			// 	$squigCanvas.css('width', $block_inner.width());
+			
+			lineWidth = 3.5; 
+			amplitude = 4.5;
 
 			$(this).css('width', $block_inner.width());
 			$(this).css('height', squiggleHeight);
-			frequency = $(this).width() * (1/26);
+			// frequency = $(this).width() * (1/26);
+			frequency = $(this).width() * (1/22);
+
+			if ($window.width() <= 768) { 
+				var sp_ratio = 1.4; 
+				lineWidth = 2.5; 
+				amplitude = amplitude / sp_ratio;
+				frequency = frequency * sp_ratio;
+			} 
+
+			squiggleHeight = (amplitude * 2) + lineWidth + 1;
 
 			var ctx = $(this)[0].getContext('2d');
 		    ctx.canvas.width  = $(this).width()*2;
 			ctx.canvas.height = $(this).height()*2;
-			ctx.scale(2,2);		// Double the dimensions for higher resolution.
+			ctx.scale(2,2);	// Double the dimensions for higher resolution.
 
-			// ctx.lineCap = 'round';
 			ctx.lineWidth = lineWidth;
 			ctx.strokeStyle = "#f32d83";
 			ctx.beginPath();
@@ -78,7 +90,20 @@ $(document).ready(function() {
 			ctx.stroke();
 		});
 	};
+
+
+// Calling Functions
+// ------------------------------------
+
+	// Call Functions
 	drawSquigglyLine();
+    changeWorkshopHeight();
+
+	// Reload Functions on Window Resize 
+	$window.resize(function () {
+        drawSquigglyLine();
+        changeWorkshopHeight();
+    });
 
 
-})(jQuery);
+});
